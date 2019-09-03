6067e90d4e941e0d4f6616398a64f0178006b9e1
LeeJerry <jerry830401@gmail.com>
2019-09-03 19:43:27 +0800

    [modfiy] webpack setting

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import $ from 'jquery'
 import 'bootstrap';
 import './index.scss'
 
-let array = ['b1', 'b2', 'b3', 's1', 's2', 's3']
-let gallery = array.map((item) => `<div class="col-4"><img src="./img/gallery/${item}.jpg"/></div>`).join("")
+import b1 from './img/gallery/b1.jpg'
+import b2 from './img/gallery/b2.jpg'
+import b3 from './img/gallery/b3.jpg'
+import s1 from './img/gallery/s1.jpg'
+import s2 from './img/gallery/s2.jpg'
+import s3 from './img/gallery/s3.jpg'
+
+let array = [b1, b2, b3, s1, s2, s3]
+let gallery = array.map((item) => `<div class="col-4"><img src="${item}"/></div>`).join("")
 
 $(document).ready(() => {
     $(".carousel").carousel({
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
 const webpack = require('webpack')
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     bundle: './src/index.js',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '/dist'),
     filename: '[name].js',
-    publicPath: '/dist/',
+    publicPath: '../dist/',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['index'],
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      title: 'yee',
+      template: './src/index.html'
+    }),
+  ],
   module: {
     rules: [
       {
@@ -25,30 +38,39 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          { loader: 'style-loader' },
+          { loader: 'style-loader' },          
           { loader: 'css-loader' },
           { loader: 'sass-loader' },
         ],
       },
       {
         test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }],
       },
       {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: 'file-loader',
             options: {
-              limit: 8192, // 8KB
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000',
-      },
+              name: '[name].[ext]',
+              outputPath: 'images/'
+            }
+          }
+        ]
+      }
     ],
   },
   optimization: {
